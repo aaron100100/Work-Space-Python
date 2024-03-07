6960959ba594de02aa6e862eae5a1f74027cde56
Aarón Meza Ramos <aaronmr01@ciencias.unam.mx>
2024-03-06 23:50:36 -0600

    Agregando el ejercicio8

---
+++ AaronMeza/Exercise8.py
@@ -0,0 +1,7 @@
+widget_peso = 75
+gizmo_peso = 112
+
+num_widget = int(input("Ingresa la cantidad de widgets del usuario: "))
+num_gizmo = int(input("Ingresa la cantidad de gizmos del usuario: "))
+print("El peso total de los widgets es: ", num_widget*widget_peso)
+print("El peso total de los gizmos es: ", num_gizmo*gizmo_peso)
